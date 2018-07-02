2b4ff0637d76f41ccbdec4034318e933171648cf
marcelooliveira <mclricardo@gmail.com>
2018-07-02 19:19:49 -0300

    Aula4 - antes

---
+++ certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/1 - Métodos Sobrecarregados/antes/Sobrecargas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class Sobrecargas : IAulaItem
+    {
+        public void Executar()
+        {
+            //VOLUME DO CUBO = lado ^ 3;
+            int lado = 3;
+
+            //VOLUME DO CILINDRO = altura * PI * raio ^ 2;
+            double raio = 2;
+            int altura = 10;
+
+            //VOLUME DO PRISMA = largura * profundidade * altura
+            long largura = 10;
+            altura = 6;
+            int profundidade = 4;
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/2 - Métodos Substituídos/antes/2 - Métodos Substituídos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class MetodosSubstituidos : IAulaItem
+    {
+        public void Executar()
+        {
+            Animal gato = new Gato() { Nome = "Bichano" };
+            gato.Beber();
+            gato.Comer();
+            gato.Andar();
+        }
+    }
+
+    class Animal
+    {
+        public String Nome { get; set; }
+
+        public void Beber()
+        {
+            Console.WriteLine("Animal.Beber");
+        }
+
+        public virtual void Comer()
+        {
+            Console.WriteLine("Animal.Comer");
+        }
+
+        public void Andar()
+        {
+            Console.WriteLine("Animal.Andar");
+        }
+    }
+
+    class Gato : Animal
+    {
+        public new void Beber()
+        {
+            Console.WriteLine("Gato.Beber");
+        }
+
+        public override void Comer()
+        {
+            Console.WriteLine("Gato.Comer");
+        }
+
+        public new void Andar()
+        {
+            Console.WriteLine("Gato.Andar");
+        }
+    }
+}
